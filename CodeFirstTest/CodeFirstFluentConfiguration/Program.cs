76dc68c91db605580e6f29f7cc5eb190345b428a
Kos <kostek001@gmail.com>
2019-01-20 17:17:30 +0300

    add CodeFirstFluentConfiguration, rename folder

---
+++ CodeFirstTest/CodeFirstFluentConfiguration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeFirstFluentConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
